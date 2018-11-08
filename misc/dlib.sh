964e45b05f4e111acf17445ad187bf3db8caf5c2
Eva <ohw0sseug@gmail.com>
2018-11-09 00:39:59 +0600

    dlib script fix and cuda path

--- misc/dlib.sh
+++ misc/dlib.sh
@@ -4,4 +4,11 @@ cd $HOME
 mkdir -p dlib
 git clone -b 'v19.9' --single-branch https://github.com/davisking/dlib.git dlib
 cd  dlib
-sudo python3 setup.py install --yes USE_AVX_INSTRUCTIONS --yes DLIB_USE_CUDA
+mkdir build
+cd build
+cmake .. -DDLIB_USE_CUDA=1 -DUSE_AVX_INSTRUCTIONS=1
+cmake --build .
+cd ..
+
+# activate virtualenv
+# python setup.py install --yes USE_AVX_INSTRUCTIONS --yes DLIB_USE_CUDA
