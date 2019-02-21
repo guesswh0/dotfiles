201097a40ccbb083a9c960767c2d02913f50c2d1
dank0 <ohw0sseug@gmail.com>
2019-02-21 23:42:38 +0600

    opencv installation script update

--- macos/opencv.sh
+++ macos/opencv.sh
@@ -16,26 +16,30 @@ brew install cmake pkg-config
 brew install jpeg libpng libtiff openexr ffmpeg
 
 # [optional] Optimization libraries
-brew install eigen tbb
+brew install eigen tbb hdf5
+
+# sphynx-doc bug 
+brew install python
+brew unlink python
 
 # Python 3.6.5_1 
 brew install \
     https://raw.githubusercontent.com/Homebrew/homebrew-core/f2a764ef944b1080be64bd88dca9a1d80130c558/Formula/python.rb
 
-cd $HOME/
-mkdir temp && cd temp
-git clone https://github.com/opencv/opencv.git
-cd opencv && git checkout tags/$opencv_version && cd $HOME/temp
+brew switch python 3.6.5_1
 
-git clone https://github.com/opencv/opencv_contrib.git
-cd opencv_contrib && git checkout tags/$opencv_version && cd $HOME/temp
+mkdir $HOME/opencv && cd $HOME/opencv
+wget -O opencv.zip https://github.com/opencv/opencv/archive/$opencv_version.zip
+wget -O opencv_contrib.zip https://github.com/opencv/opencv_contrib/archive/$opencv_version.zip
+unzip opencv.zip
+unzip opencv_contrib.zip
 
-cd opencv
+cd opencv-$opencv_version
 mkdir build && cd build
 
 cmake -DCMAKE_BUILD_TYPE=RELEASE \
     -DCMAKE_INSTALL_PREFIX=/usr/local \
-    -DOPENCV_EXTRA_MODULES_PATH=~/opencv_contrib/modules \
+    -DOPENCV_EXTRA_MODULES_PATH=~/opencv/opencv_contrib-$opencv_version/modules \
     -DPYTHON3_LIBRARY=`python3 -c 'import subprocess ; import sys ; s = subprocess.check_output("python3-config --configdir", shell=True).decode("utf-8").strip() ; (M, m) = sys.version_info[:2] ; print("{}/libpython{}.{}.dylib".format(s, M, m))'` \
     -DPYTHON3_INCLUDE_DIR=`python3 -c 'import distutils.sysconfig as s; print(s.get_python_inc())'` \
     -DPYTHON3_EXECUTABLE=`which python3` \
@@ -52,4 +56,3 @@ make -j$(sysctl -n hw.physicalcpu)
 sudo make install
 
 ln -s /usr/local/python/cv2/python-3.6/cv2.cpython-36m-darwin.so /usr/local/lib/python3.6/site-packages
-rm -rf $HOME/temp
