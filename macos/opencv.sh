d51d2522e367cdf03112dd2c9ae403f98e4010e3
dank0 <ohw0sseug@gmail.com>
2019-09-17 13:07:49 +0600

    update

--- macos/opencv.sh
+++ macos/opencv.sh
@@ -2,6 +2,12 @@
 # OpenCV installation script (macos).
 # Assume have Apple Command Line Tools $sudo xcode-select --install
 
+# references
+# https://docs.opencv.org/3.4.7/d7/d9f/tutorial_linux_install.html
+# https://stackoverflow.com/questions/33250375/compiling-opencv3-with-pyenv-using-python-3-5-0-on-osx
+# https://hackmd.io/@liangcc/BJB-Asp1-?type=view
+
+
 # Homebrew
 if ! [ -x "$(command -v brew)" ]; then
     printf "\n" | ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" &> /dev/null
@@ -9,40 +15,54 @@ fi
 
 opencv_version=3.4.7
 
+# pyenv python version
+python_version=3.6.9 
+
+# use pyenv shell
+pyenv shell $python_version
+
+# install numpy
+pip3 install numpy~=1.16.4
+
 # [required] Tools used to build and compile
 brew install cmake pkg-config
 
 # [optional*] Libraries used for image I/O operations 
 brew install jpeg libpng libtiff openexr
+# brew install ffmpeg
 
 # [optional**] Optimization libraries
-brew install eigen tbb hdf5 
+brew install eigen tbb
 
-# sphynx-doc bug
-# brew install python
-# brew unlink python
-# brew install https://raw.githubusercontent.com/Homebrew/homebrew-core/f2a764ef944b1080be64bd88dca9a1d80130c558/Formula/python.rb
-# brew switch python 3.6.5_1
+# dependencies easy way
+# brew install opencv --only-dependencies
+# brew install opencv@3 --only-dependencies
 
-mkdir $HOME/opencv && cd $HOME/opencv
+mkdir -p /usr/local/opt/opencv-$opencv_version && cd /usr/local/opt/opencv-$opencv_version
+
 wget -O opencv.zip https://github.com/opencv/opencv/archive/$opencv_version.zip
 wget -O opencv_contrib.zip https://github.com/opencv/opencv_contrib/archive/$opencv_version.zip
-unzip opencv.zip
-unzip opencv_contrib.zip
 
-cd opencv-$opencv_version
-mkdir build && cd build
+# unzip and rename
+unzip opencv.zip && mv opencv-$opencv_version opencv
+unzip opencv_contrib.zip && mv opencv_contrib-$opencv_version opencv_contrib
 
+cd opencv && mkdir build && cd build
+
+# cmake without contrib extra modules
+PREFIX=`pyenv prefix`
 cmake -DCMAKE_BUILD_TYPE=RELEASE \
-    -DCMAKE_INSTALL_PREFIX=/usr/local \
-    -DPYTHON3_LIBRARY=`python3 -c 'import subprocess ; import sys ; s = subprocess.check_output("python3-config --configdir", shell=True).decode("utf-8").strip() ; (M, m) = sys.version_info[:2] ; print("{}/libpython{}.{}.dylib".format(s, M, m))'` \
+    -DCMAKE_INSTALL_PREFIX=/usr/local/opencv/opencv-$opencv_version \
+    -DPYTHON3_EXECUTABLE=$PREFIX/bin/python3 \
+    -DPYTHON3_LIBRARY=`python3 -c 'import subprocess; import sys; s = subprocess.check_output("python3-config --configdir", shell=True).decode("utf-8").strip(); (M, m) = sys.version_info[:2]; print("{}/libpython{}.{}m.dylib".format(s, M, m))'` \
     -DPYTHON3_INCLUDE_DIR=`python3 -c 'import distutils.sysconfig as s; print(s.get_python_inc())'` \
-    -DPYTHON3_EXECUTABLE=`which python3` \
+    -DWITH_TBB=ON \
+    -DWITH_EIGEN=ON \
     -DBUILD_opencv_python2=OFF \
     -DBUILD_opencv_python3=ON ..
 
+# to build with contrib add
+# OPENCV_EXTRA_MODULES_PATH=/usr/local/opencv/opencv-$opencv_version/opencv_contrib/modules
+
 make -j$(sysctl -n hw.physicalcpu)
 sudo make install
-
-# link to destination site-package
-# ln -s /usr/local/lib/python3.6/site-packages/cv2/python-3.6/cv2.cpython-36m-darwin.so /path/dest/site-package/
