1d561d6e2eb64a89b8f7f8be0c2183718e9f37f7
dank0 <ohw0sseug@gmail.com>
2020-12-22 19:03:17 +0600

    updated brew installation path

--- macos/main.sh
+++ macos/main.sh
@@ -3,7 +3,7 @@
 # install brew
 if ! [ -x "$(command -v brew)" ]; 
 then
-        /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+        /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 else
         echo "brew already installed!"
 fi
