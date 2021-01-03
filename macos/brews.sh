6ca86bf5ce3cb0626bfa3c0b4ec5f38614f1c2a1
dank0 <ohw0sseug@gmail.com>
2021-01-03 15:20:22 +0600

    updated brew casks

--- macos/brews.sh
+++ macos/brews.sh
@@ -11,7 +11,6 @@ brew install zsh
 brew install git
 brew install tree
 brew install wget
-brew install brew-cask-completion
 brew install source-highlight
 
 # Additional utilities
@@ -27,13 +26,14 @@ brew install readline
 brew install python
 
 # Applications
-brew cask install iterm2
-brew cask install transmission
-brew cask install smcfancontrol
-brew cask install dropbox
-brew cask install iina
+brew install --cask iterm2
+brew install --cask transmission
+brew install --cask smcfancontrol
+brew install --cask dropbox
+brew install --cask iina
+brew install --cask ngrok
 
 # fonts
-brew cask install font-hack-nerd-font
+brew install --cask font-hack-nerd-font
 
 brew cleanup
